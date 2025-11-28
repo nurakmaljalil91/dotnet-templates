ced2e6f0f4c65fe7b8c9f9218b2dc7656b7a2acf
Nur Akmal Jalil <48422687+nurakmaljalil91@users.noreply.github.com>
2025-11-28 22:40:29 +0800

    Switch audit dates to NodaTime Instant type
    Replaced DateTime with NodaTime Instant for CreatedDate and UpdatedDate in BaseAuditableEntity to improve date/time handling. Added related NuGet packages to Application and Infrastructure projects for NodaTime and PostgreSQL support.

--- WebApplicationTemplate/src/Domain/Common/BaseAuditableEntity.cs
+++ WebApplicationTemplate/src/Domain/Common/BaseAuditableEntity.cs
@@ -1,10 +1,12 @@
-﻿namespace Domain.Common;
+﻿using NodaTime;
+
+namespace Domain.Common;
 
 public abstract class BaseAuditableEntity : BaseEntity
 {
-    public DateTime CreatedDate { get; set; }
+    public Instant CreatedDate { get; set; }
     public string? CreatedBy { get; set; }
-    public DateTime UpdatedDate { get; set; }
+    public Instant UpdatedDate { get; set; }
     public string? UpdatedBy { get; set; }
 }
 
