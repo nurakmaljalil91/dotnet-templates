548aacf4650fdbd0f53957227ac608775ae8697d
Nur Akmal Jalil <48422687+nurakmaljalil91@users.noreply.github.com>
2025-11-30 13:05:01 +0800

    Integrate Serilog logging into WebAPI project
    Added Serilog configuration to appsettings.json and updated Program.cs to use Serilog for structured logging. Installed Serilog and related packages in the project file. This enhances logging capabilities with multiple sinks, enrichers, and improved log output for diagnostics and monitoring.

--- WebApplicationTemplate/src/WebAPI/Program.cs
+++ WebApplicationTemplate/src/WebAPI/Program.cs
@@ -1,23 +1,60 @@
+using Serilog;
+using Serilog.Events;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Read Serilog configuration from appsettings
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting.Diagnostics", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    var dateTimeUtcNow = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+
+    Log.Information("Starting application...");
+
+    Log.Information("UTC Time: {DateTimeUtcNow}", dateTimeUtcNow);
+
+    builder.Host.UseSerilog();
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+    });
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    // Ensure fatal startup errors are captured
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    // Flush and close loggers
+    Log.CloseAndFlush();
+}
